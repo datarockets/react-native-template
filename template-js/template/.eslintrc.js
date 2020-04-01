9c8027cfb6da872fbaa0468f24093b7167a82052
Pavel Vashkel <p.vashkel@datarockets.com>
2020-04-01 12:31:23 +0300

    Setup ESLint config

--- template-js/template/.eslintrc.js
+++ template-js/template/.eslintrc.js
@@ -1,7 +1,20 @@
 module.exports = {
   root: true,
-  extends: 'airbnb',
+  extends: [
+    'airbnb',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:react-hooks/recommended',
+  ],
   parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+  },
+  env: {
+    'react-native/react-native': true
+  },
   plugins: [
     'react',
     'react-native',
@@ -9,4 +22,28 @@ module.exports = {
     'jsx-a11y',
     'react-hooks',
   ],
+  rules: {
+    'arrow-body-style': 'off',
+    'consistent-return': 'off',
+    'default-case': 'off',
+    'semi': ['error', 'never'],
+    'import/extensions': 'off',
+    'import/prefer-default-export': 'off',
+    'react/jsx-tag-spacing': ['error', {
+      'closingSlash': 'never',
+      'beforeSelfClosing': 'never',
+      'afterOpening': 'never',
+      'beforeClosing': 'allow',
+    }],
+    'react/jsx-filename-extension': ['error', {
+      'extensions': [
+        '.js',
+      ],
+    }],
+    'react-native/no-unused-styles': 'error',
+    'react-native/no-inline-styles': 'error',
+    'react-native/no-color-literals': 'error',
+    'react-native/no-raw-text': 'error',
+    'react-native/no-single-element-style-arrays': 'error',
+  }
 }
