c03f7205a33195215e33dbfc546e8acfae47bd9a
Pavel Vashkel <p.vashkel@datarockets.com>
2020-04-10 10:30:08 +0300

    Add MainNavigator declaration

---
+++ template-ts/template/src/navigation/MainNavigator.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import HomeScreen from '../screens/HomeScreen'
+
+type MainStackParamList = {
+  HomeScreen: undefined,
+}
+
+const Stack = createStackNavigator<MainStackParamList>()
+
+const MainNavigator: React.FC = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="HomeScreen">
+        <Stack.Screen name="HomeScreen" component={HomeScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default MainNavigator
