2a86b570cf9cdd2230898ce9abebeaf690195938
Pavel Vashkel <p.vashkel@datarockets.com>
2020-04-10 17:51:32 +0300

    Add types for selectors

--- template-ts/template/src/models/Counter/selectors.ts
+++ template-ts/template/src/models/Counter/selectors.ts
@@ -1,6 +1,10 @@
-const getState = (state) => state.counter
+import { RootState } from '../index'
 
-const counterAmountSelector = (state) => getState(state).amount
+import { CounterState } from './types'
+
+const getState = (state: RootState): CounterState => state.counter
+
+const counterAmountSelector = (state: RootState): number => getState(state).amount
 
 export default {
   counterAmountSelector,
