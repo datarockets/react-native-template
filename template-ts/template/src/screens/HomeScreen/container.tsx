a928a5619ef635146b34f360f8853f497e379b2e
Pavel Vashkel <p.vashkel@datarockets.com>
2020-04-10 10:25:26 +0300

    Add example HomeScreeen

---
+++ template-ts/template/src/screens/HomeScreen/container.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import HomeScreenView from './view'
+
+interface HomeScreenContainerProps {
+}
+
+const HomeScreenContainer: React.FC<HomeScreenContainerProps> = () => {
+  return (
+    <HomeScreenView/>
+  )
+}
+
+export default HomeScreenContainer
---
+++ template-ts/template/src/screens/HomeScreen/view.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+interface HomeScreenViewProps {
+}
+
+const HomeScreenView: React.FC<HomeScreenViewProps> = () => {
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+    </View>
+  )
+}
+
+export default HomeScreenView
