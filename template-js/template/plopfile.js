16aa41fa15831561856b7cdd05c576ffc56afba2
Sanzhar Zholdiyarov <zholdiyarov@gmail.com>
2020-04-15 18:29:18 +0600

    Feature. Components and screens generators (#28)
    * added component and screen generator
    
    * Update package.json
    
    * fix for eslint issues
    
    * added separate view to screen
    
    Co-authored-by: Pavel Vashkel <jgugn1@gmail.com>

--- template-js/template/plopfile.js
+++ template-js/template/plopfile.js
@@ -49,5 +49,65 @@ module.exports = function (plop) {
         templateFile: 'templates/model/types.js',
       },
     ],
+  }),
+  plop.setGenerator('component', {
+    description: 'generates a functional component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Name a new component. NOTE: manually register the new component in the src/components/index.js.',
+        validate(value) {
+          return (/.+/).test(value) ? true : 'name is required'
+        },
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        skipIfExists: true,
+        path: 'src/components/{{ pascalCase name }}/index.js',
+        templateFile: 'templates/component/index.js',
+      },
+      {
+        type: 'add',
+        skipIfExists: true,
+        path: 'src/components/{{ pascalCase name }}/styles.js',
+        templateFile: 'templates/component/styles.js',
+      },
+    ],
+  }),
+  plop.setGenerator('screen', {
+    description: 'generates a screen component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Name a new screen. NOTE: manually register the new component in the src/screens/index.js.',
+        validate(value) {
+          return (/.+/).test(value) ? true : 'name is required'
+        },
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        skipIfExists: true,
+        path: 'src/screens/{{ pascalCase name }}/index.js',
+        templateFile: 'templates/screen/index.js',
+      },
+      {
+        type: 'add',
+        skipIfExists: true,
+        path: 'src/screens/{{ pascalCase name }}/styles.js',
+        templateFile: 'templates/screen/styles.js',
+      },
+      {
+        type: 'add',
+        skipIfExists: true,
+        path: 'src/screens/{{ pascalCase name }}/view.js',
+        templateFile: 'templates/screen/view.js',
+      },
+    ],
   })
 }
---
+++ template-js/template/src/screens/Sample/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import View from './view'
+
+const SampleScreen = () => {
+  return (
+    <View/>
+  )
+}
+
+
+SampleScreen.propTypes = {
+}
+
+SampleScreen.defaultProps = {
+}
+
+export default SampleScreen
